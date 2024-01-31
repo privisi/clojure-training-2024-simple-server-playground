f7111b2cf000bf4a4d8fbc15c42dd7a0972895d3
Privisi <BZhang705@hotmail.com>
2024-01-31 22:05:48 +0900

    Explain how to destructure routes.

--- src/simple_server/core.clj
+++ src/simple_server/core.clj
@@ -6,16 +6,87 @@
    [ring.mock.request :as mock]
    [ring.util.response :refer [response created redirect not-found status]]))
 
-;;; Responses
-;;;
-;; Now that we've got some sensible routing going on,
-;; let's look at our responses for a minute.
+;;; Destructuring routes and parameters
+;;; Let's see what else we can do with routes.
+
+;; Remember, we currently have this:
+
+(comment
+  (defroutes game-routes
+    (GET "/new-game" [] (new-game-handler)) ; Why does one have it wrapped in (),
+    (GET "/guess"    [] guess-handler)      ; and this one doesn't?
+    (ANY "*"         [] (not-found "Sorry, No such URI on this server!"))))
+
+;; What are the empty [], and why do we have (new-game-handler) vs guess-handler ?
+(response "Here is some text.")
+;;
+;; HTTP is often (usually?) used to transfer some specific information,
+;; either in the form of query-strings, or post data.  What we had to do
+;; with extract-guess is tedious, and we can tell we're going to need to
+;; do this a LOT.  So compojure gives us some pretty powerful tools for this.
 
 
-;; We have a lot of code which returns maps which look like this:
-{:status 200
- :headers {"Content-Type" "text/plain"}
- :body "OK- start guessing at /guess"}
+;;;; Destructuring the request object.
+
+;; What is in the empty [] is actually a destructuring form which
+;; gets applied to the (possibly modified) incoming request.
+
+((GET "/foo" request {:status 200 :body request})
+ (mock/request :get "/foo?name=Bob"))
+
+
+;; We can apply the standard map destructuring constructs:
+((GET "/foo" {headers :headers} {:status 200 :body headers})
+ (mock/request :get "/foo?name=Bob"))
+
+;; or
+((GET "/foo"
+   {:keys [server-name request-method uri]}
+   {:status 200 :body [server-name request-method uri]})
+ (mock/request :get "/foo?name=Bob"))
+
+
+;;;; Extracting parts of the URI
+
+(defn greet-user-handler [name]
+  (response (format "Greetings, %s!" name)))
+
+((GET "/foo/:name" [name] (greet-user-handler name))
+ (mock/request :get "/foo/Sally"))
+
+;; Compojure, if it sees a vector as the destructuring construct,
+;; takes things one step further, and does some magic behind the
+;; scenes; in this case, binds any keyword part in the URI path
+;; to a symbol of the same name.
+
+;; This is nice, because now we can see how we can start separating
+;; our functions which do logic from those that do "http stuff";
+;; in particular, we could rewrite above like this:
+
+(defn greet-user [name]
+  (format "Greetings, %s!" name))
+
+((GET "/foo/:name" [name] (response (greet-user name)))
+ (mock/request :get "/foo/Sally"))
+;; {:status 200, :headers {}, :body "Greetings, Sally!"}
+
+;; And, it turns out, if we just return a string, Compojure will interpret
+;; that as a success response, so we can actually just write this¹:
+
+((GET "/foo/:name" [name] (greet-user name))
+ (mock/request :get "/foo/Sally"))
+;; {:status 200, :headers {"Content-Type" "text/html; charset=utf-8"}, :body "Greetings, Sally!"}
+
+
+;; 2. extracting query string parameters
+
+;; It would be nice if we could get the data from the query string
+;; or post data in a similar way, i.e. write:
+
+((GET "/foo" [name] {:status 200 :body (str "The name is: " name)})
+ (mock/request :get "/foo?name=Bob"))
+
+;; To get this to work, we need to discuss /middlewares/.
 
 
 ;; ring.util.response provides some handy shortcuts for creating these maps; e.g.
