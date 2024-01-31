e3446cfb937de4cdd4aec15e023f4bde278201da
Privisi <BZhang705@hotmail.com>
2024-01-31 21:41:14 +0900

    Implement simple guessing game

--- src/simple_server/core.clj
+++ src/simple_server/core.clj
@@ -3,12 +3,54 @@
   (:use ring.adapter.jetty))
 
 
-(defn handler [request]
-  (clojure.pprint/pprint request)
-  {:status 200
+(defn no-such-uri-response []
+  {:status  404
    :headers {"Content-Type" "text/plain"}
-   :body "Hello, class!2"})
+   :body    "Sorry, No such URI on this server!"})
+
+(def game-in-progress (atom nil))
+
+(defn extract-guess [qs]
+  (when (seq qs)
+    (Long. (second (clojure.string/split qs #"=")))))
+
+(defn guess-response [request]
+  (if-let [guess (extract-guess (:query-string request))]
+    (cond
+      (= guess @game-in-progress)
+      (and (reset! game-in-progress (+ 1 (rand-int 10)))
+           {:status 200
+            :headers {"Content-Type" "text/plain"}
+            :body "Congratulations! You win!"})
+
+      (< guess @game-in-progress)
+      {:status 200
+       :headers {"Content-Type" "text/plain"}
+       :body "Too low."}
+
+      (> guess @game-in-progress)
+      {:status 200
+       :headers {"Content-Type" "text/plain"}
+       :body "Too high."})
+    {:status 400                        ; Bad request
+     :headers {"Content-Type" "text/plain"}
+     :body "You need to supply a guess with /guess?guess=N"}))
+
+(defn new-game-response []
+  ;; Make our new game:
+  (reset! game-in-progress (+ 1 (rand-int 10)))
+  {:status  200
+   :headers {"Content-Type" "text/plain"}
+   :body    "OK- start guessing at /guess"})
 
 
-(def server
-  (run-jetty #'handler {:port 3001 :join false}))
+(defn handler [request]
+  #_(clojure.pprint/pprint request)
+  (condp = (:uri request)
+    "/new-game" (new-game-response)
+    "/guess"    (guess-response request)
+    (no-such-uri-response)))
+
+
+(defonce server
+  (future (run-jetty #'handler {:port 3001 :join false})))
